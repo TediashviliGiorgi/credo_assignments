e95b86b7d9370335778b05779c5fce8f3f4f63e2
Giorgi <exchangeinvestment@yahoo.com>
2022-10-24 21:39:54 +0400

    initial commit

---
+++ fibo/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace fibo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static long  Fib (int n)
+               
+            {
+                
+                if (n == 0)
+                {
+                    return 0;
+                }
+                else if (n == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return Fib(n - 1) + Fib(n - 2);
+                }
+            }
+            Console.WriteLine("Please enter position");
+            int n = Convert.ToInt32(Console.ReadLine());
+            n = n - 1;
+            Console.WriteLine(Fib(n));
+        }
+    }
+}
