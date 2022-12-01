8e61de747b0a257e29a8e27710e06b050a72114d
TediashviliGiorgi <81388116+TediashviliGiorgi@users.noreply.github.com>
2022-12-02 01:47:17 +0400

    initial commit

---
+++ ExtensionFunctions/MyExtension.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+
+namespace ExtensionFunctions
+{
+    public static class MyExtension
+    {
+        //IsNumber()
+        public static bool IsNumber(this string param)
+        {
+            if (Regex.IsMatch(param, @"^\d+$"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //IsDate()
+        public static bool IsDate(this string param)
+        {
+            string[] formats = {"M/d/yyyy h:mm:ss tt", "M/d/yyyy h:mm tt",
+                   "MM/dd/yyyy hh:mm:ss", "M/d/yyyy h:mm:ss",
+                   "M/d/yyyy hh:mm tt", "M/d/yyyy hh tt",
+                   "M/d/yyyy h:mm", "M/d/yyyy h:mm",
+                   "MM/dd/yyyy hh:mm", "M/dd/yyyy hh:mm"};
+
+            foreach (string format in formats)
+            {
+                if (DateTime.TryParseExact(param, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out _) == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //ToWords()
+        public static string[] ToWords(this string param)
+        {
+            string[] words = param.Split(' ', ',', '.', '?', ':');
+            return words;
+        }
+
+        //CalculateHash()
+        public static string CalculateHash(this string param)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(param);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+
+        //SaveToFile(string filePath, string txt)
+        public static void SaveToFile(string filePath, string fileName, string txt)
+        {
+            string filePathWithName = $"{filePath}\\{fileName}.txt";
+            using (var sw = new StreamWriter(filePathWithName, true))
+            {
+                sw.WriteLine(txt);
+                Console.WriteLine($"Text succesfully saved in {filePath}");
+            }
+        }
+    }
+}
---
+++ ExtensionFunctionsCaller/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExtensionFunctions;
+namespace ExtensionFunctionsCaller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // IsNumber()
+            Console.WriteLine("123".IsNumber());
+
+            //IsDate()
+            Console.WriteLine("05/01/2009 6:32:05 PM".IsDate());
+
+            //ToWords()
+            string txt = "Is this a word counter? yes it is";
+            Console.WriteLine(txt.ToWords().Length);
+
+            //CalculateHash() 
+            Console.WriteLine(txt.CalculateHash());
+
+            //SaveToFile(string filePath, string txt)
+            string filePath = "D:\\importentFiles\\trainedProjects\\credo-assignments\\ExtensionFunctionsCaller";
+            MyExtension.SaveToFile(filePath, "FirstFile", "Test Text");
+        }
+    }
+}
