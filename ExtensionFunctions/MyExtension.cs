42857bd4dd6a8e4ee633f45a3fa6dfe9ed5e830a
TediashviliGiorgi <81388116+TediashviliGiorgi@users.noreply.github.com>
2022-12-02 04:18:12 +0400

    initial commit

--- ExtensionFunctions/MyExtension.cs
+++ ExtensionFunctions/MyExtension.cs
@@ -4,6 +4,7 @@ using System.Text.RegularExpressions;
 using System.Security.Cryptography;
 using static System.Net.Mime.MediaTypeNames;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace ExtensionFunctions
 {
@@ -71,6 +72,121 @@ namespace ExtensionFunctions
                 sw.WriteLine(txt);
                 Console.WriteLine($"Text succesfully saved in {filePath}");
             }
+        }
+
+        //Encrypt
+        public static string Encrypt(string key, string txt)
+        {
+            string EncryptionKey = key;
+            byte[] clearBytes = Encoding.Unicode.GetBytes(txt);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    txt = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return txt;
+        }
+
+        //Decrypt
+        public static string Decrypt(string key, string txt)
+        {
+            string EncryptionKey = key;
+            txt = txt.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(txt);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    txt = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return txt;
+        }
+
+        //ToPercent()
+        public static double ToPercent(this double param)
+        {
+            return param * 100;
+        }
+
+        //RoundDown()
+        public static int RoundDown(double param)
+        {
+            int number = (int)Math.Floor(param);
+            return number;
+        }
+
+        //ToDecimal()
+        public static decimal ToDecimal(int param)
+        {
+            decimal number = (decimal)param;
+            return number;
+        }
+
+        //IsGreater(double number
+        public static bool IsGreater(this double param, double number)
+        {
+           if(param > number)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //IsLess(double number)
+        public static bool IsLess(this double param, double number)
+        {
+            if (param < number)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Min(DateTime date)
+        public static DateTime Min(DateTime d1, DateTime d2)
+        {
+            int minDate = DateTime.Compare(d1, d2);
+            
+            if (minDate < 0 )
+            {
+                return d2;
+            }
+            else
+            {
+                return d1;  
+            }
+        }
+
+        //BeginingOfMonth()
+        public static DateTime BeginigOfMonth(this DateTime param)
+        {
+            return new DateTime(param.Year, param.Month, 1);
+        }
+
+        //EndOfMonth()
+        public static DateTime EndOfMonth(this DateTime param)
+        {
+            var firstDayOfMonth = new DateTime(param.Year, param.Month, 1);
+            return firstDayOfMonth.AddMonths(1).AddDays(-1);
         }
     }
 }
--- ExtensionFunctionsCaller/Program.cs
+++ ExtensionFunctionsCaller/Program.cs
@@ -21,6 +21,36 @@ namespace ExtensionFunctionsCaller
             //SaveToFile(string filePath, string txt)
             string filePath = "D:\\importentFiles\\trainedProjects\\credo-assignments\\ExtensionFunctionsCaller";
             MyExtension.SaveToFile(filePath, "FirstFile", "Test Text");
+
+            //Encrypt
+            Console.WriteLine(MyExtension.Encrypt("abc123", "Test Text"));
+
+            //Decrypt ???
+            //Console.WriteLine(MyExtension.Decrypt("abc123", "894f666ce383ad5f5316c4861831efbdd63fa9654c947a9b496e269632a4b026"));
+
+            //ToPercent()
+            Console.WriteLine(0.5.ToPercent());
+
+            //RoundDown()
+            Console.WriteLine(MyExtension.RoundDown(37.7));
+
+            //ToDecimal()
+            Console.WriteLine(MyExtension.ToDecimal(5).GetType());
+
+            //IsGreater(double number
+            Console.WriteLine(2.2.IsGreater(10.8));
+
+            //IsLess(double number)
+            Console.WriteLine(2.2.IsLess(10.8));
+
+            //Min(DateTime date)
+            Console.WriteLine(MyExtension.Min(new DateTime(2022, 12, 22), new DateTime(2022, 12, 22)));
+
+            //BeginingOfMonth()
+            Console.WriteLine(MyExtension.BeginigOfMonth(new DateTime(2022, 11, 23)));
+
+            //EndOfMonth()
+            Console.WriteLine(MyExtension.EndOfMonth(new DateTime(2022, 11, 23)));
         }
     }
 }
