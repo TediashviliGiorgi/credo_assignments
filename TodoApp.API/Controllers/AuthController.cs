24e4861ae9220e7a21e69a58ec21d6c336a32297
TediashviliGiorgi <81388116+TediashviliGiorgi@users.noreply.github.com>
2023-01-24 04:42:02 +0400

    jwt authentication

---
+++ TodoApp.API/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase 
+    {
+        [HttpGet]
+        public string Ping()
+        {
+            return "Pong";
+        }
+    }
+}
